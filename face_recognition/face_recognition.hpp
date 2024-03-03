2eb080a3e0996016fe0038148c9f6f06ea0d5ed8
Artem Dyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2024-03-03 18:28:32 +0300

    Refactoring and solution of moving in the extreme position of head

--- face_recognition/face_recognition.hpp
+++ face_recognition/face_recognition.hpp
@@ -5,36 +5,42 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 
+#include "serial_connector/serial_connector.hpp"
+
 namespace camera
 {
     class CameraCV
     {
     private:
-        int k;
         int face_x, face_y;
-        double leg_x, leg_y, dist, side;
         int angle_x, angle_y;
-        unsigned int cam_index;
+        int cam_index;
         std::string com_port;
         cv::Mat img;
         cv::CascadeClassifier facedetect;
 
-    public:
-        CameraCV(unsigned int index, std::string port);
-
-        ~CameraCV() {}
-
-        // функция поворота изображения
-        cv::Mat rotate(const cv::Mat& src, double angle);
+    private:
+        // повернуть изображение
+        static cv::Mat rotate(const cv::Mat& src, double angle);
 
         // получить координаты центра
-        cv::Point2f get_center_rect(cv::Point2f tl, cv::Point2f br);
+        static cv::Point2f get_center_rect(cv::Point2f tl, cv::Point2f br);
 
-        // функция нахождения индекса самого переднего лица
-        int get_front_face_index(std::vector<cv::Rect>& faces);
+        // найти индекс самого переднего лица
+        static int get_front_face_index(std::vector<cv::Rect>& faces);
 
-        // функция определения координат наклоненного лица
-        void determ_true_face_coord(std::vector<cv::Rect>& faces, const int front_face_index, int angle, const int video_width, int const video_height);
+        // определить координаты наклоненного лица
+        static void determ_true_face_coord(std::vector<cv::Rect>& faces, int front_face_index, int angle, int video_width, int video_height);
+
+        // найти лица
+        static void find_faces(cv::CascadeClassifier& facedetect, const cv::Mat& img, std::vector<cv::Rect_<int>>& faces, int& angle);
+
+        // определить добавочные углы для поворота приводов
+        static void determ_additional_angles(std::vector<cv::Rect_<int>>& faces, int front_face_index, cv::Point img_center, int& angle_x, int& angle_y);
+
+    public:
+        CameraCV(int index, std::string port);
+        ~CameraCV() = default;
 
         void execute();
     };
