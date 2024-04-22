8f802237bf0072cae02b13d5ee1bb67ac139ca73
Liash66 <AKoshelev2004@yandex.ru>
2024-04-22 22:20:34 +0200

    add sound playing

--- robot/Robot.cpp
+++ robot/Robot.cpp
@@ -16,6 +16,10 @@
 #include <thread>
 #include <chrono>
 #include <utility>
+#include <Windows.h>
+#include <mmsystem.h>
+
+#pragma comment(lib, "winmm.lib")
 
 namespace robot
 {
@@ -34,6 +38,9 @@ namespace robot
         io::SerialConnector* m_sc;
         m_sc = new io::SerialConnector(m_com_port, 9600); // последовательный порт
         m_sc->Open();
+
+        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+        std::chrono::seconds reset_time(4); // Время, через которое нужно обнулить таймер
 
         std::this_thread::sleep_for(std::chrono::seconds(3));
 
@@ -57,10 +64,23 @@ namespace robot
             int angle = 0; // угол наклона лица
             find_faces(facedetect, img, faces, angle);
 
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end - start;
+
             // Если лицо обнаружено
             if (!faces.empty())
             {
                 int front_face_index = get_front_face_index(faces); // индекс ближайшего лица
+
+                std::cerr << faces[front_face_index].height;
+
+                if ((elapsed_seconds > reset_time) && (faces[front_face_index].height > 145))
+                {
+                    // Обнуляем таймер и выводим сообщение
+                    PlaySound(TEXT("zayac_chort.wav"), NULL, SND_ASYNC);
+                    start = std::chrono::steady_clock::now();
+                }
+
                 determ_true_face_coord(faces, front_face_index, angle, video_width, video_height);
 
                 k_eye = 0.45 * (faces[front_face_index].br().x - faces[front_face_index].tl().x);
@@ -71,6 +91,7 @@ namespace robot
                 std::cerr << faces[front_face_index].br().x - faces[front_face_index].tl().x << std::endl;
 
                 faces[front_face_index].x += k_eye; // обратный сдвиг
+
                 for (size_t i = 0; i < faces.size(); i++)
                 {
                     if (i == front_face_index)
@@ -80,9 +101,9 @@ namespace robot
                     else
                     {
                         rectangle(img, faces[i].tl(), faces[i].br(), cv::Scalar(0, 255, 0), 5); // построение рамок остальных лиц
-					}
+                    }
                 }
-				
+
                 // В пределах допустимых углов поворота головы
                 if (((m_angle_x + m_add_angle_x) >= -45 && (m_angle_x + m_add_angle_x) <= 45) && ((m_angle_y + m_add_angle_y) >= -20 && (m_angle_y + m_add_angle_y) <= 20))
                 {
